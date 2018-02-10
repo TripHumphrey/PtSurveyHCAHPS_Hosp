9d4cfa2a0db6b0e2c9a3f112516e94360a1f7bff
Trip Humphrey <trip_humphrey@bshsi.org>
2018-02-10 13:21:57 -0500

    Local Initial

---
+++ main.R
@@ -0,0 +1,63 @@
+# Include neccesary packages
+library(readr)    # To read csv data
+library(dplyr)    # To manipulate data
+library(magrittr) # for piping functionality
+library(tidyr)    # to create tidy data
+library(stringr)  # for string functionality
+library(ggplot2)  # for visualization
+
+# Placeholder to store api query in future development
+# 
+# Until API is functional data should be downloaded from https://data.medicare.gov/Hospital-Compare/Patient-survey-HCAHPS-Hospital/dgck-syfz
+# Click Export and save as CSV
+#
+# load raw data
+hcahps_raw <- read_csv("Patient_survey__HCAHPS__-_Hospital.csv",
+                       col_types = cols(
+                        `HCAHPS Answer Percent` = col_number(), 
+                        `HCAHPS Answer Percent Footnote` = col_skip(),
+                        `HCAHPS Linear Mean Value` = col_number(),
+                        `Measure End Date` = col_date(format = "%m/%d/%Y"),
+                        `Measure Start Date` = col_date(format = "%m/%d/%Y"),
+                        `Number of Completed Surveys` = col_number(),
+                        `Number of Completed Surveys Footnote` = col_skip(),
+                        `Patient Survey Star Rating` = col_number(),
+                        `Patient Survey Star Rating Footnote` = col_skip(),
+                        `Survey Response Rate Percent` = col_number(),
+                        `Survey Response Rate Percent Footnote` = col_skip())
+                       )
+
+# Need to convert numeric data into appropriate class
+
+
+# Spread Row Level LInear response data to wide format
+hcahps_wide_resp <- hcahps_raw %>%
+  filter(!str_detect(`HCAHPS Measure ID`, "LINEAR|STAR")) %>%
+  select(`Provider ID`, `HCAHPS Measure ID`, `HCAHPS Answer Percent`) %>%
+  spread('HCAHPS Measure ID', 'HCAHPS Answer Percent')
+
+# Spread Row Level Star response data to wide format
+hcahps_wide_linear <- hcahps_raw %>%
+  filter(str_detect(`HCAHPS Measure ID`, "LINEAR")) %>%
+  select(`Provider ID`, `HCAHPS Measure ID`, `HCAHPS Linear Mean Value`) %>%
+  spread('HCAHPS Measure ID', 'HCAHPS Linear Mean Value')
+
+# Spread Row level Leikert response data to wide format
+#
+# For some reason, this leikert data is split into separate categories as separate measures rather than single variable factor data
+hcahps_wide_star <- hcahps_raw %>%
+  filter(str_detect(`HCAHPS Measure ID`, "STAR")) %>%
+  select('Provider ID',`HCAHPS Measure ID`, `Patient Survey Star Rating`) %>%
+  spread('HCAHPS Measure ID', `Patient Survey Star Rating`)
+
+# Create a new dataframe to join with the wide datasets
+hcahps_wide <- hcahps_raw %>% 
+  select(`Provider ID`,`Hospital Name`, Address, City, State, `ZIP Code`,
+         `County Name`, `Phone Number`, Location, `Number of Completed Surveys`,
+         `Survey Response Rate Percent`, `Measure Start Date`, `Measure End Date`) %>% 
+  distinct() %>%
+  left_join(hcahps_wide_resp, by = "Provider ID") %>%
+  left_join(hcahps_wide_linear, by = "Provider ID") %>%
+  left_join(hcahps_wide_star, by = "Provider ID")
+
+
